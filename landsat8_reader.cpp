38f7e5ab954dfa483b712b4f6510745d2e0afa1a
Bill Yau <billyauhk@gmail.com>
2014-03-06 21:32:57 +0800

    Investigating memory efficiency of a OpenCV based approach.

--- landsat8_reader.cpp
+++ landsat8_reader.cpp
@@ -15,52 +15,73 @@ And, at last, write out pixels with valid observation into a file...spatialLite/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 using namespace cv;
 using namespace std;
 
 int main(int argc, char* argv[]){
   GDALDataset *poDataset = NULL;
-  char* filename;
+  char *basename, *filename;
+  uint16_t basenameLength;
   Mat imageBuffer;
   uint64_t xsize, ysize;
+  uint8_t band_number;
 
 // Check arguments
   if(argc<2){
     fprintf(stderr,"Not enough argument!\n");exit(-1);
   }
-  filename = argv[1];
-
-// Open file
-  GDALAllRegister();
-  poDataset = (GDALDataset*) GDALOpen(filename, GA_ReadOnly);
-  if(poDataset == NULL){
-    fprintf(stderr,"FIle cannot be opened!\n");exit(-1);
+  basename = argv[1];
+  basenameLength = strlen(basename);
+  filename = (char*) malloc(sizeof(char)*(basenameLength+10));
+  if(!filename){
+    fprintf(stderr,"Cannot allocate memory!\n");exit(-1);
   }
 
-// Print some metadata
-  printf("Filename: %s\n", argv[1]);
-  xsize = poDataset->GetRasterXSize();
-  ysize = poDataset->GetRasterYSize();
-  printf("Raster Size: %d x %d\n", xsize, ysize);
-  printf("Raster Count: %d\n", poDataset->GetRasterCount());
+// Prepare the GDAL driver
+  GDALAllRegister();
 
-// Read Image data
-  imageBuffer.create(ysize, xsize, CV_32FC1);
-  poDataset->RasterIO(GF_Read, 0, 0, xsize, ysize,
-                      (void*) imageBuffer.ptr(0), xsize, ysize,
-                      GDT_Float32, 1, NULL, 0, 0, 0);
+  for(band_number=1;band_number<=11;band_number++){
+    // Construct file name
+    strcpy(filename, basename);
+    sprintf(filename+basenameLength, "_B%d.TIF", band_number);
 
-// One should be able to get coordinate with GDALApplyGeoTransform() (or maybe also GDALInvGeoTransform())
+    // Open file
+      poDataset = (GDALDataset*) GDALOpen(filename, GA_ReadOnly);
+      if(!poDataset){
+        fprintf(stderr,"File cannot be opened!\n");exit(-1);
+      }
 
-// Display the data
-  imageBuffer.convertTo(imageBuffer, CV_16UC1);
-  normalize(imageBuffer, imageBuffer, 0, 65535, NORM_MINMAX);
-  namedWindow("Thumbnail",CV_GUI_EXPANDED);
-  imshow("Thumbnail", imageBuffer);
-  waitKey(0);
-  destroyWindow("Thumbnail");
+    // Print some metadata
+      printf("Filename: %s\n", filename);
+      xsize = poDataset->GetRasterXSize();
+      ysize = poDataset->GetRasterYSize();
+      printf("Raster Size: %d x %d\n", xsize, ysize);
+      printf("Raster Count: %d\n", poDataset->GetRasterCount());
 
-  delete poDataset;
+    // Read Image data
+      imageBuffer.create(ysize, xsize, CV_32FC1);
+      poDataset->RasterIO(GF_Read, 0, 0, xsize, ysize,
+                          (void*) imageBuffer.ptr(0), xsize, ysize,
+                          GDT_Float32, 1, NULL, 0, 0, 0);
+
+    // One should be able to get coordinate with GDALApplyGeoTransform() (or maybe also GDALInvGeoTransform())
+
+    // Adjust the radiance as told by the metadata file
+
+    // Display the data if it is not band 8 (panochromatic band takes too much RAM)
+      imageBuffer.convertTo(imageBuffer, CV_16UC1);
+      normalize(imageBuffer, imageBuffer, 0, 65535, NORM_MINMAX);
+      namedWindow("Thumbnail",CV_GUI_EXPANDED);
+      imshow("Thumbnail", imageBuffer);
+      waitKey(0);
+      destroyWindow("Thumbnail");
+
+      imageBuffer.release(); // Complusorily free memory
+      delete poDataset; poDataset = NULL;
+  }
+  free(filename);
+
   return 0;
 }
